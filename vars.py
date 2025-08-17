17e30bc3a7cb468380eccb9eedc9f816da40886d
haklesh051 <hakleshbairwa051@gmail.com>
2025-08-17 14:55:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
+api_id = 27730810 #change_with_your_Api_id.
+api_hash = "dadc82d593e3f5334da107b63ced36b9" #change_with_your_Api_hash.
+bot_token = "8401715922:AAHqb1ef9b_njgXjralFJ_pyvGy7vmMsdeI" #change_with_your_bot_token.
+auth_users = [6132794263] #change_with_your_user_id.
 sudo_user = 1713924419 #change_with_your_user_id.
 log_channel = -1002154281532 #change_with_your_channel_id.
 txt_channel = -1002154281532 #change_with_your_channel_id.
+
